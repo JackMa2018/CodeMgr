6e683728c949942801e2552f8dfbb3e18b0ae380
lvchunhui <lvchunhui@biocome.com>
2020-10-14 17:22:39 +0800

    1111111

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleApp1
             Console.WriteLine("dev分支添加02");
             Console.WriteLine("主分支添加01");
             Console.WriteLine("dev2分支添加01");
+            //tag之后的更新的内容
+            Console.WriteLine("dev1合并之后添加内容");
             Console.WriteLine("Hello World!");
         }
     }
